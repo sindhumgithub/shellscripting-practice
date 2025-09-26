20b02de9751b7e235cff5fc437bd9eafbe5e4d2f
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-09-26 12:14:15 +0530

    6th commit

--- 2-countlines.sh
+++ 2-countlines.sh
@@ -10,8 +10,12 @@ cat $filename
 #Checking whether the file exist or not
 if [ -e $filename ]; then
     echo "File $filename exists"
-    result=$(wc -l < $filename)
-    echo "The number of lines in a file is:" $result
+    result1=$(wc -l < $filename)
+    echo "The number of lines in a file is:" $result1
+    result2=$(wc -w < $filename)
+    echo "The number of words in a file is:" $result2
+    result3=$(wc -c < $filename)
+    echo "The number of lines in a file is:" $result3
     exit 0
 else
      echo "File $filename does not exists"
