5b74dba44d5cbebdcfabec3ad10d7c7da22a8d8f
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-09-26 14:14:22 +0530

    14th commit

--- 1-samplefilep.sh
+++ 1-samplefilep.sh
@@ -5,26 +5,36 @@ filename=$1
 
 if [ -e $filename ]; then   #-e file exists or not exists.
     echo -e "$G $filename file exists $N"
-if [ -r $filename ]; then   #  -r indicates whether the file is readable or not.
-    echo -e "$G $filename is readable $N"
-else
-    echo -e "$R $filename is NOT readable $N"
-fi
+    
+    # shell scripting code whether the file is readable or not 
+    if [ -r $filename ]; then   #  -r indicates whether the file is readable or not.
+        echo -e "$G $filename is readable $N"
+    else
+        echo -e "$R $filename is NOT readable $N"
+    fi
 
-if [ -w $filename ]; then     #  -w incidates whether the file is writable or not.
-    echo -e "$G $filename file is writable $N"
-else
-    echo -e "$R $filename file NOT writable $N"
-fi
+    # shell scripting code whether the file is writable or not 
+    if [ -w $filename ]; then     #  -w incidates whether the file is writable or not.
+        echo -e "$G $filename file is writable $N"
+    else
+        echo -e "$R $filename file NOT writable $N"
+    fi
 
-if [ -x $filename ]; then  #  -x incidates whether the file is executable or not.
-    echo -e "$G $filename file is executable $N"
-else
-    echo -e "$R $filename is NOT executable $N"
-fi
+    # shell scripting code whether the file is executable or not 
+    if [ -x $filename ]; then  #  -x incidates whether the file is executable or not.
+        echo -e "$G $filename file is executable $N"
+    else
+        echo -e "$R $filename is NOT executable $N"
+    fi
 
+elif [ -d $filename ]; then
+    echo "Directory $filename exists"
+    result=$(cat $filename | ls -ltrh)
+    echo "Files present in $filename directory is::" $result
+    
 else
     echo -e "$R $filename DOES NOT exist $N"
+
 fi
 
 
