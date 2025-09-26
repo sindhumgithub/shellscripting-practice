4e1051fe0c91bd1d693c9632e5f1c3efb67b3adf
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-09-26 13:58:01 +0530

    13th commit

--- 1-samplefilep.sh
+++ 1-samplefilep.sh
@@ -4,23 +4,23 @@ G="\e[32m"
 filename=$1
 
 if [ -e $filename ]; then   #-e file exists or not exists.
-    echo "$G $filename file exists $N"
+    echo -e "$G $filename file exists $N"
 if [ -r $filename ]; then   #  -r indicates whether the file is readable or not.
-    echo "$G $filename is readable $N"
+    echo -e "$G $filename is readable $N"
 else
-    echo "$R $filename is NOT readable $N"
+    echo -e "$R $filename is NOT readable $N"
 fi
 
 if [ -w $filename ]; then     #  -w incidates whether the file is writable or not.
-    echo "$G $filename file is writable $N"
+    echo -e "$G $filename file is writable $N"
 else
-    echo "$R $filename file NOT writable $N"
+    echo -e "$R $filename file NOT writable $N"
 fi
 
 if [ -x $filename ]; then  #  -x incidates whether the file is executable or not.
-    echo "$G $filename file is executable $N"
+    echo -e "$G $filename file is executable $N"
 else
-    echo "$R $filename is NOT executable $N"
+    echo -e "$R $filename is NOT executable $N"
 fi
 
 else
