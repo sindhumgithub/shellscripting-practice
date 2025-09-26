237aaea555a7f7630aef6bb47d19c38e6fb4d591
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-09-26 13:38:16 +0530

    10th commit

--- 1-filepermissions.sh
+++ 1-filepermissions.sh
@@ -4,27 +4,27 @@ read -p "Please enter your filename::" filename
 echo "Your filename is:: " $filename
 
 if [ -e $filename ]; then   #-e file exists or not exists.
-    echo "$filename file exists"
+    echo "$G $filename file exists $N"
 if [ -r $filename ]; then   #  -r indicates whether the file is readable or not.
-    echo "$filename is readable"
+    echo "$G $filename is readable $N"
 else
-    echo "$filename is NOT readable"
+    echo "$R $filename is NOT readable $N"
 fi
 
-if [ -w $filename ]; then 
-    echo "$filename file is writable"
+if [ -w $filename ]; then     #  -w incidates whether the file is writable or not.
+    echo "$G $filename file is writable $N"
 else
-    echo "$filename file NOT writable"
+    echo "$R $filename file NOT writable $N"
 fi
 
-if [ -x $filename ]; then
-    echo "$filename file is executable"
+if [ -x $filename ]; then  #  -x incidates whether the file is executable or not.
+    echo "$G $filename file is executable $N"
 else
-    echo "$filename is NOT executable"
+    echo "$R $filename is NOT executable $N"
 fi
 
 else
-    echo "$filename DOES NOT exist"
+    echo -e "$R $filename DOES NOT exist $N"
 fi
 
 
