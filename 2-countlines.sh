fc5358953dee3e0ee8dc93d3e167457e2c4e531d
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-09-26 11:53:58 +0530

    3rd commit

--- 2-countlines.sh
+++ 2-countlines.sh
@@ -10,7 +10,7 @@ cat $filename
 #Checking whether the file exist or not
 if [ -e $filename ]; then
     echo "File $filename exists"
-    result=$(wc -l)
+    result=$(wc -l $filename)
     echo "The word count is:" $result
 else
      echo "File $filename does not exists"
